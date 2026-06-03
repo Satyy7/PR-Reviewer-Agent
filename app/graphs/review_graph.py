ef42717ace63f6a738bb07805e4654f8a300302e
Satyy7 <saisatyavardhan2004@gmail.com>
2026-06-04 00:49:50 +0530

    Built review_graph

--- app/graphs/review_graph.py
+++ app/graphs/review_graph.py
@@ -1,5 +1,7 @@
-from langgraph.graph import StateGraph
-from langgraph.graph import END
+from langgraph.graph import (
+    StateGraph,
+    END
+)
 
 from app.schemas.review_state import (
     ReviewState
@@ -24,3 +26,64 @@ from app.agents.architecture_agent import (
 from app.agents.aggregate_agent import (
     aggregate_agent
 )
+
+
+builder = StateGraph(
+    ReviewState
+)
+
+builder.add_node(
+    "security",
+    security_agent
+)
+
+builder.add_node(
+    "performance",
+    performance_agent
+)
+
+builder.add_node(
+    "quality",
+    quality_agent
+)
+
+builder.add_node(
+    "architecture",
+    architecture_agent
+)
+
+builder.add_node(
+    "aggregate",
+    aggregate_agent
+)
+
+builder.set_entry_point(
+    "security"
+)
+
+builder.add_edge(
+    "security",
+    "performance"
+)
+
+builder.add_edge(
+    "performance",
+    "quality"
+)
+
+builder.add_edge(
+    "quality",
+    "architecture"
+)
+
+builder.add_edge(
+    "architecture",
+    "aggregate"
+)
+
+builder.add_edge(
+    "aggregate",
+    END
+)
+
+review_graph = builder.compile()
