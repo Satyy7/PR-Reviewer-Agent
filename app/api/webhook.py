7b63162e30ea8eaa5192f28afa5f83399c9fe9cb
Satyy7 <saisatyavardhan2004@gmail.com>
2026-06-03 15:16:57 +0530

    Pull Request info extractor added

--- app/api/webhook.py
+++ app/api/webhook.py
@@ -1,25 +1,9 @@
-# from fastapi import APIRouter
-
-# from app.services.github_service import GitHubService
-
-# router = APIRouter()
-
-
-# @router.post("/github")
-# async def github_webhook(payload: dict):
-
-#     pr_info = GitHubService.extract_pr_info(payload)
-
-#     print(pr_info)
-
-#     return {
-#         "message": "received",
-#         "data": pr_info
-#     }
-
 from fastapi import APIRouter, Request
 
+from app.services.github_service import GitHubService
+
 router = APIRouter()
+
 
 @router.post("/github")
 async def github_webhook(request: Request):
@@ -28,9 +12,12 @@ async def github_webhook(request: Request):
 
     event_type = request.headers.get("X-GitHub-Event")
 
-    print("=" * 100)
-    print("EVENT:", event_type)
-    print("PAYLOAD KEYS:", payload.keys())
-    print("=" * 100)
+    if event_type == "pull_request":
+
+        pr_info = GitHubService.extract_pr_info(payload)
+
+        print("=" * 80)
+        print(pr_info)
+        print("=" * 80)
 
     return {"message": "received"}
--- app/services/github_service.py
+++ app/services/github_service.py
@@ -6,6 +6,18 @@ from app.core.config import settings
 class GitHubService:
 
     @staticmethod
+    def extract_pr_info(payload: dict):
+
+        return {
+            "action": payload["action"],
+            "pr_number": payload["number"],
+            "repo_name": payload["repository"]["name"],
+            "owner": payload["repository"]["owner"]["login"],
+            "head_branch": payload["pull_request"]["head"]["ref"],
+            "base_branch": payload["pull_request"]["base"]["ref"]
+        }
+
+    @staticmethod
     def get_authenticated_user():
 
         response = requests.get(
