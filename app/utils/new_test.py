2154404acafca1a440906bc0ab597d3633c20a90
Satyy7 <saisatyavardhan2004@gmail.com>
2026-06-03 16:18:33 +0530

    new dummy test code

--- app/utils/new_test.py
+++ app/utils/new_test.py
@@ -1,2 +1,24 @@
 def test():
     return "This is a test function for GitHub integration."
+
+def generate_user_report(users):
+    report = {
+        "total_users": len(users),
+        "active_users": 0,
+        "inactive_users": 0,
+        "premium_users": 0,
+        "emails": []
+    }
+
+    for user in users:
+        if user["active"]:
+            report["active_users"] += 1
+        else:
+            report["inactive_users"] += 1
+
+        if user.get("premium", False):
+            report["premium_users"] += 1
+
+        report["emails"].append(user["email"])
+
+    return report
